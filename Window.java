14754bfedddc1f08963e7c1659eca4aa2d21a008
Abdelrahmanhassanjsdnjsdp <160647102+Abdelrahmanhassanjsdnjsdp@users.noreply.github.com>
2024-11-19 14:52:32 +0200

    Add files via upload

---
+++ Window.java
@@ -0,0 +1,56 @@
+
+//Class for JFrame extension.
+import javax.swing.*;
+
+
+/*/
+ * Window that holds the Display JPanel.
+ */
+public class Window extends JFrame {
+
+	//Variable for final JFrame size.
+	private final int HEIGHT = 800;
+	private final int WIDTH = 800;
+
+
+	/**
+	 * Default constructor.
+	 */
+	Window(boolean pause) {
+
+		//Set the title.
+		setTitle("Frogger Remix");
+
+		//Set the size of the JFrame.
+		setSize(WIDTH, HEIGHT);
+
+		//Set window to screen center.
+		setLocationRelativeTo(null);
+
+		//Specify the close button action.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//set resize.
+		setResizable(false);
+
+		//Add panel to frame.
+		add(new Display(pause));
+
+		//Display the window.
+		setVisible(true);
+	}
+
+
+	/**
+	 * Main constructor to start program.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Pause game if first run.
+		final boolean pause = true;
+
+		//Create window for game.
+		new Window(pause);
+	}
+}
